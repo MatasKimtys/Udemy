c2d13cc32fa7abc4345dc0895c834af0111d952d
Matas Kimtys <93611099+MatasKimtys@users.noreply.github.com>
2022-07-16 19:17:36 +0100

    Update rpg.cpp

--- rpg.cpp
+++ rpg.cpp
@@ -4,13 +4,15 @@ A simple rpg example.
 
 Task:
 add button listeners to console
-depending on pressed button, information of monster, player should be displayed seperately. 
-make an attack button. 
+depending on pressed button, information of monster, player should be displayed seperately.
+make an attack button.
 
 information of monster should consist of name, level, atk_min, atk_max, health, evasion, defence, defence rate.
-information of player should consist of level, atk_min, atk_max, health, evasion, defence, defence rate. 
+information of player should consist of level, atk_min, atk_max, health, evasion, defence, defence rate.
 -------------------------*/
 //https://github.com/MatasKimtys/Portfolio.git
+
+
 
 #include <iostream>
 #include <thread>
@@ -25,70 +27,164 @@ information of player should consist of level, atk_min, atk_max, health, evasion
 #include <climits>
 #include <cmath>
 #include <vector>
+#include "Monster.h"
+#include "Player.h"
+#include "Weapon.h"
+
+using namespace std;
+using namespace std::this_thread; //sleep_for, sleep_until
+using namespace std::chrono_literals; //ns, us, ms, s, h, etc.
+using std::chrono::system_clock;
 
 //initialise functions
-// 
+Monster createMonster();
+Weapon createWeapon();
+Player createPlayer();
+Monster attackMonster();
+Monster monsterThread();
+Player playerThread();
 //initialise global variables
-bool _playing = true;
-int main(){
+bool _playing = false;
+bool _lobby = false;
+bool __player_stats = false;
+bool _duel = false;
+
+string _action;
+
+int main() {
     srand(time(NULL));
-    Monster* _object = new Monster();
-    std::cout << _object->_monster_name;
+    Monster _monster = createMonster();
+    Player _player = createPlayer();
+    std::thread _m(monsterThread());
+    std::thread _p(playerThread());
+    _playing = true;
+    while (_playing) {
+        _lobby = true;
+        while (_lobby) {
+            cout << "What now? Actions: (player stats/duel/quit) ";
+            cin >> _action;
+            if (_action == "player stats") {
+
+            }
+            else if (_action == "duel") {
+                sleep_for(500ms);
+                _duel = true;
+                _lobby = false;
+            }
+            else if (_action == "quit") {
+                _lobby = false;
+                _playing = false;
+            }
+            else {
+                cout << "I dont understand. Type (player stats OR duel)";
+                sleep_for(1500ms);
+                Clear();
+            }
+        }
+
+        while (_duel) {
+            while (_monster._monster_health > 0 && _player._player_health > 0) {
+
+                sleep_for(10ms);
+            }
+        }
+
+    }
+
     return 0;
 }
 
-class Monster {
-public:
-    std::string _monster_name = ""; //0
-    int _monster_level; //1
-    int _monster_atk_min; //2
-    int _monster_atk_max; //3
-    int _monster_health; //4 
-    int _monster_evasion; //5
-    int _monster_evasion_rate; //6
-    int _monster_defence; //7 
-    int _monster_damage_reduction_rate; //8
+void monsterThread(Monster _input_monster) {
+    while (_input_monster._monster_health > 0) {
 
-    Monster() {
-        Monster _monster_to_create;
-        std::vector<std::string> __random_names =
-        {
-            "Babirusa",
-            "Baboon",
-            "Bactrian Camel",
-            "Badger",
-            "Baird’s Rat Snake",
-            "Bald Eagle",
-            "Baleen Whale",
-            "Balinese",
-            "Balkan Lynx",
-            "Ball Python"
-        };
-        int _random_number = rand() % (__random_names.size() - 0 + 1) + 0;
-        std::string _random_name = __random_names.at(_random_number); // pick a random name from list of monster name list
-
-
-        _monster_to_create._monster_name = _random_name;
-        _monster_to_create._monster_level = rand() % (100 - 1 + 1) + 1;
-        _monster_to_create._monster_atk_min = rand() % (_monster_to_create._monster_level / 2 - 1 + 1) + 1;
-        _monster_to_create._monster_atk_max = rand() % (_monster_to_create._monster_level * 2 - _monster_to_create._monster_atk_min + 1) + _monster_to_create._monster_atk_min;
-        _monster_to_create._monster_health = rand() % (_monster_to_create._monster_level * 10 - _monster_to_create._monster_level * 5 + 1) + _monster_to_create._monster_level * 5;
-        _monster_to_create._monster_evasion = rand() % (_monster_to_create._monster_level - 0 + 1) + 0;
-        _monster_to_create._monster_evasion_rate = _monster_to_create._monster_evasion / 10;
-        _monster_to_create._monster_defence = _monster_to_create._monster_level - (_monster_to_create._monster_level / 10);
-        _monster_to_create._monster_damage_reduction_rate = _monster_to_create._monster_level / 10;
     }
-};
+}
+
+void playerThread(Player _input_player) {
+    while (_input_player._player_health > 0) {
+
+    }
+}
 
 
+Monster createMonster() {
+    Monster _monster_to_create;
+    std::vector<std::string> __random_names =
+    {
+        "Babirusa",
+        "Baboon",
+        "Bactrian Camel",
+        "Badger",
+        "Baird’s Rat Snake",
+        "Bald Eagle",
+        "Baleen Whale",
+        "Balinese",
+        "Balkan Lynx",
+        "Ball Python"
+    };
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    int _random_number = rand() % (__random_names.size() - 0 + 1) + 0;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    _monster_to_create._monster_name = __random_names.at(_random_number);
+    _monster_to_create._monster_level = rand() % (100 - 1 + 1) + 1;
+    _monster_to_create._monster_atk_min = rand() % (_monster_to_create._monster_level / 2 - 1 + 1) + 1;
+    _monster_to_create._monster_atk_max = rand() % (_monster_to_create._monster_level * 2 - _monster_to_create._monster_atk_min + 1) + _monster_to_create._monster_atk_min;
+    _monster_to_create._monster_health = rand() % (_monster_to_create._monster_level * 10 - _monster_to_create._monster_level * 5 + 1) + _monster_to_create._monster_level * 5;
+    _monster_to_create._monster_evasion = rand() % (_monster_to_create._monster_level - 0 + 1) + 0;
+    _monster_to_create._monster_evasion_rate = _monster_to_create._monster_evasion / 10;
+    _monster_to_create._monster_defence = _monster_to_create._monster_level - (_monster_to_create._monster_level / 10);
+    _monster_to_create._monster_damage_reduction_rate = _monster_to_create._monster_level / 10;
+    _monster_to_create._monster_experience_reward = rand() % (_monster_to_create._monster_level * 2 - _monster_to_create._monster_level / 2 + 1) + _monster_to_create._monster_level / 2;
+    return _monster_to_create;
+}
+
+Weapon createWeapon() {
+    Weapon _weapon;
+    std::vector<std::string> __material =
+    {
+        "Wooden", "Steel", "Gold", "Platinum", "Obsidian", "Plaglass"
+    };
+    std::vector<std::string> __weapon_names =
+    {
+        "Cestus", "Longsword", "Warhammer", "Bow", "Crossbow", "War Axe", "Rod"
+    };
+
+    int _random1 = rand() % (__material.size() - 0 + 1) + 0;
+    int _random2 = rand() % (__weapon_names.size() - 0 + 1) + 0;
+
+    _weapon._weapon_name = __material.at(_random1) + __weapon_names.at(_random2);
+    _weapon._weapon_accuracy = 70;
+    _weapon._weapon_description = "It's a weapon.";
+    _weapon._weapon_durability = 100;
+    _weapon._weapon_physical_atk_min = 10;
+    _weapon._weapon_physical_atk_max = 30;
+    _weapon._weapon_magic_atk_min = 10;
+    _weapon._weapon_magic_atk_max = 30;
+    _weapon._weapon_enchancement_level = 0;
+
+    return _weapon;
+}
+
+Player createPlayer() {
+    Player _player;
+    cout << "Whats your name: ";
+    cin >> _player._player_name;
+    _player._player_level = 1;
+    _player._player_health = 100;
+    _player._player_stamina = 100;
+    _player._player_mana = 100;
+    _player._player_exp = 0;
+    _player._equipped_weapon = createWeapon();
+
+    return _player;
+}
+
+void Clear()
+{
+
+#if defined _WIN32
+    system("cls");
+#endif
+
+}
+
